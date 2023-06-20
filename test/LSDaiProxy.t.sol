8a3893ed732df16763e7c5e2e65439eedb9599c1
Tsunade <0xtsunade@proton.me>
2023-06-20 13:29:43 +0100

    chore: proxy tests

--- test/LSDaiProxy.t.sol
+++ test/LSDaiProxy.t.sol
@@ -2,27 +2,55 @@
 pragma solidity 0.8.20;
 
 // upgradeable proxy
-import {TransparentUpgradeableProxy} from "@openzeppelin/contracts/proxy/transparent/TransparentUpgradeableProxy.sol";
-import {ProxyAdmin} from "@openzeppelin/contracts/proxy/transparent/ProxyAdmin.sol";
+import {TransparentUpgradeableProxy} from '@openzeppelin/contracts/proxy/transparent/TransparentUpgradeableProxy.sol';
+import {ProxyAdmin} from '@openzeppelin/contracts/proxy/transparent/ProxyAdmin.sol';
 
-import {LSDAITestBase, MAKER_POT} from "./common/LSDAITestBase.sol";
-import {LSDai} from "../contracts/LSDai.sol";
-import {ILSDai} from "../contracts/interfaces/ILSDai.sol";
+import {LSDAITestBase, MAKER_POT} from './common/LSDAITestBase.sol';
+import {LSDai} from '../contracts/LSDai.sol';
+import {ILSDai} from '../contracts/interfaces/ILSDai.sol';
 
 contract LSDaiTests is LSDAITestBase {
-  function test_CanBeUsedInProxy() public {
-    LSDai lsdai = new LSDai();
-    // initialize LSDai
-    // lsdai.initialize();
+  LSDai lsdaiImplementation;
+  ProxyAdmin proxyAdmin;
+  LSDai lsdaiProxy;
 
+  // LSDai users
+  address lsdTripper = address(0x23);
+  address lsdEnjoyer = address(0x24);
+  address lsdDreamer = address(0x25);
+
+  function setUp() public {
     // deploy proxy admin
-    ProxyAdmin proxyAdmin = new ProxyAdmin();
+    proxyAdmin = new ProxyAdmin();
 
+    lsdaiImplementation = new LSDai();
+    // deploy proxy admin
+    proxyAdmin = new ProxyAdmin();
     // deploy proxy
-    TransparentUpgradeableProxy tuProxy = new TransparentUpgradeableProxy(address(lsdai), address(proxyAdmin), '');
-
+    TransparentUpgradeableProxy tuProxy = new TransparentUpgradeableProxy(
+      address(lsdaiImplementation),
+      address(proxyAdmin),
+      ''
+    );
     // initialize proxy
-    ILSDai lsdaiProxy = ILSDai(address(tuProxy));
+    lsdaiProxy = LSDai(address(tuProxy));
+
+    uint256 _depositCap = 100_000 ether;
+    uint256 _interestFee = 250;
+    uint256 _withdrawalFee = 1;
+    address _feeRecipient = address(this);
+    lsdaiProxy.initialize(_depositCap, _interestFee, _withdrawalFee, _feeRecipient);
+  }
+
+  function test_CanBeUsedInProxy() public {
+    // deploy proxy
+    TransparentUpgradeableProxy tuProxy = new TransparentUpgradeableProxy(
+      address(lsdaiImplementation),
+      address(proxyAdmin),
+      ''
+    );
+    // initialize proxy
+    lsdaiProxy = LSDai(address(tuProxy));
 
     uint256 _depositCap = 5000;
     uint256 _interestFee = 100;
@@ -36,8 +64,31 @@ contract LSDaiTests is LSDAITestBase {
     assertEq(lsdaiProxy.interestFee(), _interestFee);
     assertEq(lsdaiProxy.withdrawalFee(), _withdrawalFee);
     assertEq(lsdaiProxy.feeRecipient(), _feeRecipient);
-
     assertEq(lsdaiProxy.totalSupply(), 0);
     assertEq(lsdaiProxy.totalShares(), 0);
+  }
+
+  function test_UserCanWithdrawRecievedLSDAI() public {
+    mintDAI(lsdTripper, 10_000 ether);
+
+    uint256 initialDeposit = dai.balanceOf(lsdTripper);
+    // Deposit everything into LSDai
+    depositDAI(lsdaiProxy, lsdTripper, initialDeposit);
+    assertEq(dai.balanceOf(lsdTripper), 0, 'LSD tripper should have 0 DAI');
+    // 1:1 ratio
+    assertEq(lsdaiProxy.balanceOf(lsdTripper), initialDeposit, 'LSD tripper should have initial deposit');
+    assertEq(lsdaiProxy.balanceOf(lsdEnjoyer), 0, 'LSD Enjoyer should have 0 LSDai');
+
+    vm.warp(block.timestamp + 52 weeks);
+    lsdaiProxy.rebase();
+
+    // Take 1000 LSDAI from LSD tripper and give it to LSD Enjoyer
+    uint256 amountToTransfer = 1000 ether;
+    vm.prank(lsdTripper);
+    lsdaiProxy.transfer(lsdEnjoyer, amountToTransfer);
+    // assertEq(lsdai.balanceOf(lsdEnjoyer), amountToTransfer, 'LSD Enjoyer should have 1000 LSDai');
+
+    // LSD enjoyer withdraws 1000 LSDai
+    withdrawDAI(lsdaiProxy, lsdEnjoyer, lsdaiProxy.balanceOf(lsdEnjoyer));
   }
 }
--- test/common/LSDAITestBase.sol
+++ test/common/LSDAITestBase.sol
@@ -1,10 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.20;
 
-import {Test} from "forge-std/Test.sol";
+import {Test} from 'forge-std/Test.sol';
 
-import {IDai} from "../../contracts/interfaces/IDai.sol";
-import {LSDai} from "../../contracts/LSDai.sol";
+import {IDai} from '../../contracts/interfaces/IDai.sol';
+import {LSDai} from '../../contracts/LSDai.sol';
 
 address constant DAI_ADDRESS = address(0x6B175474E89094C44Da98b954EedeAC495271d0F);
 address constant MAKER_POT = address(0x197E90f9FAD81970bA7976f33CbD77088E5D7cf7);
@@ -34,24 +34,38 @@ contract LSDAITestBase is Test, TestUtils {
    */
   function getMakerDaiSavingsRate() public returns (uint256) {
     bytes32 dsr = vm.load(address(MAKER_POT), bytes32(uint256(MAKER_POT_DSR_SLOT)));
-    emit log_named_uint("MakerDAO Dai Savings Rate", uint256(dsr));
+    emit log_named_uint('MakerDAO Dai Savings Rate', uint256(dsr));
 
     return uint256(dsr);
+  }
+
+  function depositDAI(LSDai lsdai, address account, uint256 daiAmount) public {
+    vm.startPrank(account);
+    // maximum allowance
+    dai.approve(address(lsdai), type(uint256).max);
+    lsdai.deposit({daiAmount: daiAmount, to: account});
+    vm.stopPrank();
+  }
+
+  function withdrawDAI(LSDai lsdai, address account, uint256 daiAmount) public {
+    vm.startPrank(account);
+    lsdai.withdraw(daiAmount);
+    vm.stopPrank();
   }
 
   /**
    * @dev Log LSDai metrics.
    */
   function logLSDAIMetrics(LSDai lsdai, string memory _header) public {
-    string memory header = bytes(_header).length > 0 ? string.concat(" ", _header, " ") : "";
+    string memory header = bytes(_header).length > 0 ? string.concat(' ', _header, ' ') : '';
 
-    emit log_string(string.concat("------- LSDai Metrics", header, "-------"));
-    emit log_named_decimal_uint("LSDAI total supply", lsdai.totalSupply(), 18);
-    emit log_named_decimal_uint("LSDAI protcol fees (fee feeRecipient)", lsdai.balanceOf(lsdai.feeRecipient()), 18);
-    emit log_named_decimal_uint("LSDAI total supply according to DSR", lsdai.getTotalPotSharesValue(), 18);
-    emit log_named_decimal_uint("LSDAI pot shares using pot.pie()", lsdai.potShares(), 18);
-    emit log_string("");
-    emit log_string("");
+    emit log_string(string.concat('------- LSDai Metrics', header, '-------'));
+    emit log_named_decimal_uint('LSDAI total supply', lsdai.totalSupply(), 18);
+    emit log_named_decimal_uint('LSDAI protcol fees (fee feeRecipient)', lsdai.balanceOf(lsdai.feeRecipient()), 18);
+    emit log_named_decimal_uint('LSDAI total supply according to DSR', lsdai.getTotalPotSharesValue(), 18);
+    emit log_named_decimal_uint('LSDAI pot shares using pot.pie()', lsdai.potShares(), 18);
+    emit log_string('');
+    emit log_string('');
   }
 
   /**
@@ -62,13 +76,13 @@ contract LSDAITestBase is Test, TestUtils {
     uint256 shares = lsdai.sharesOf(account);
 
     // Logging
-    emit log_string(string.concat("------- ", accountName, " Metrics -------"));
-    emit log_named_decimal_uint(string.concat(accountName, " LSDAI balance"), balance, 18);
-    emit log_named_decimal_uint(string.concat(accountName, " LSDAI shares"), shares, 18);
+    emit log_string(string.concat('------- ', accountName, ' Metrics -------'));
+    emit log_named_decimal_uint(string.concat(accountName, ' LSDAI balance'), balance, 18);
+    emit log_named_decimal_uint(string.concat(accountName, ' LSDAI shares'), shares, 18);
     // emit log_named_decimal_uint('balance shares', lsdai.getSharesByPooledDai(balance), 18);
-    emit log_named_decimal_uint(string.concat(accountName, " DAI balance"), dai.balanceOf(account), 18);
+    emit log_named_decimal_uint(string.concat(accountName, ' DAI balance'), dai.balanceOf(account), 18);
 
-    emit log_string("");
-    emit log_string("");
+    emit log_string('');
+    emit log_string('');
   }
 }
