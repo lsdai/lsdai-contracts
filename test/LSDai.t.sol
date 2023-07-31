f56b2c6113b5eada7b7ae78287552e20f81b3d39
Tsunade <0xtsunade@proton.me>
2023-07-31 10:47:07 +0100

    chore: update tests

--- test/LSDai.t.sol
+++ test/LSDai.t.sol
@@ -359,9 +359,9 @@ contract LSDaiTests is LSDAITestBase {
     // Set withdrawal fee to 0.03%
     lsdai.setWithdrawalFee(3);
     assertEq(lsdai.withdrawalFee(), 3, 'Withdrawal fee should be 0.03%');
-    // Revert if the fee is greater than 0.05%
+    // Revert if the fee is greater than 1%
     vm.expectRevert(LSDai.LSDai__WithdrawalFeeHigh.selector);
-    lsdai.setWithdrawalFee(6);
+    lsdai.setWithdrawalFee(101); // 1.01%
   }
 
   function test_setInterestFee() public {
